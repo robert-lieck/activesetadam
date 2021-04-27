e0afa1836e886e160a9ad89603290e1d8b9a5eba
Robert Lieck <science@robert-lieck.com>
2021-04-27 21:49:26 +0200

    multiple improvements
    - l1-log-regularisation independent of l1 (l1_log and log_scale parameter)
    - gradient of l1 terms for zero parameters properly treated

--- asadam/ASAdam.py
+++ asadam/ASAdam.py
@@ -23,7 +23,7 @@ class ASAdam(Optimizer):
     """
 
     def __init__(self, params, lr=1e-3, betas=(0.9, 0.999, 0.9), eps=1e-8,
-                 l1=None, glue=None, safety=1., active=None, sub_uncertain=False, log_scale=None,
+                 l1=None, glue=None, l1_log=None, log_scale=None, safety=1., active=None, sub_uncertain=False,
                  max_activation=None, min_steps=0,
                  weight_decay=0, amsgrad=False, debug=False):
         """
@@ -42,16 +42,18 @@ class ASAdam(Optimizer):
         :param active: (None/True/False; default: None) Whether dimensions are initially active or not. For None, any
         dimension is inactive [active] if its initial value is zero [non-zero].
         :param sub_uncertain: (bool; default: False) Whether to subtract the uncertainty from the gradient
+        :param l1_log: (None or float >= 0.; default None) L1 regularisation strength with log decay, which will be
+        added to the gradient (see log_scale for details).
         :param log_scale: (None or float > 0; default: None) instead of a plain L1 regularisation add a term
-        l1 * s * log(1 + |x_i|/s), where s is the log_scale. This corresponds to a gradient of
-        l1 * sign(x_i)/(1 + |x_i|/s). That is for |x_i| << s it behaves like a normal L1 regularisation with strength
-        l1 but then flattens out. For log_scale --> inf, it is approaching a standard L1 regularisation, which exactly
-        realised with the default value of None. This also adds a concave component to the loss function, which may help
-        increasing sparsity in case of redundant parameters. For instance, if the loss only depends on the sum x_i + x_j
-        of two parameters, both have the exact same gradient everywhere. While only one would need to be non-zero, they
-        will be update jointly and never separate. Even if they have slightly different values there is not component of
-        the gradient pushing one down and the other up. The log_scale parameter is effectively adding such a component.
-        To break the symmetry and induce an initial disbalance, the max_activation and min_steps parameters can be used.
+        l1_log * s * log(1 + |x_i|/s), where s is the log_scale. This corresponds to a gradient of
+        l1_log * sign(x_i)/(1 + |x_i|/s). That is for |x_i| << s it behaves like a normal L1 regularisation with
+        strength l1_log but then flattens out. For log_scale --> inf, it is approaching a standard L1 regularisation.
+        This also adds a concave component to the loss function, which may help increasing sparsity in case of redundant
+        parameters. For instance, if the loss only depends on the sum x_i + x_j of two parameters, both have the exact
+        same gradient everywhere. While only one would need to be non-zero, they will be update jointly and never
+        separate. Even if they have slightly different values there is not component of the gradient pushing one down
+        and the other up. The log_scale parameter is effectively adding such a component. To break the symmetry and
+        induce an initial disbalance, the max_activation and min_steps parameters can be used.
         :param max_activation: (None or int > 0; default: None) Maximal number of dimensions activated per iteration. If
         more dimensions than allowed surpass the threshold of l1 + glue, the ones with the highest gradient will be
         activated. Ties are broken randomly.
@@ -72,8 +74,13 @@ class ASAdam(Optimizer):
             raise ValueError("Invalid L1 value: {}".format(l1))
         if glue is not None and not torch.all(0.0 <= torch.tensor(glue)):
             raise ValueError("Invalid glue value: {}".format(glue))
+        if l1_log is not None and not torch.all(0.0 <= torch.tensor(l1_log)):
+            raise ValueError("Invalid L1-log value: {}".format(l1_log))
         if log_scale is not None and not torch.all(0.0 <= torch.tensor(log_scale)):
             raise ValueError("Invalid log_scale value: {}".format(log_scale))
+        if (l1_log is None) != (log_scale is None):
+            raise ValueError(f"l1_log and log_scale have to be either both None or both provided, "
+                             f"but are {l1_log} and {log_scale}")
         if max_activation is not None and not 0.0 <= max_activation:
             raise ValueError("Invalid max_activation value: {}".format(max_activation))
         if not 0 <= min_steps:
@@ -87,7 +94,8 @@ class ASAdam(Optimizer):
         if not 0.0 <= weight_decay:
             raise ValueError("Invalid weight_decay value: {}".format(weight_decay))
         defaults = dict(lr=lr, betas=betas, eps=eps,
-                        l1=l1,  glue=glue, safety=safety, active=active, sub_uncertain=sub_uncertain, log_scale=log_scale,
+                        l1=l1,  glue=glue, l1_log=l1_log, log_scale=log_scale,
+                        safety=safety, active=active, sub_uncertain=sub_uncertain,
                         max_activation=max_activation, min_steps=min_steps,
                         weight_decay=weight_decay, amsgrad=amsgrad)
         super(ASAdam, self).__init__(params, defaults)
@@ -118,7 +126,8 @@ class ASAdam(Optimizer):
                 if grad.is_sparse:
                     raise RuntimeError('ASAdam does not support sparse gradients, please consider SparseAdam instead')
                 amsgrad = group['amsgrad']
-                l1, glue, log_scale = group['l1'], group['glue'], group['log_scale']
+                l2 = group['weight_decay']
+                l1, glue, l1_log, log_scale = group['l1'], group['glue'], group['l1_log'], group['log_scale']
                 max_activation, min_steps = group['max_activation'], group['min_steps']
                 beta1, beta2, beta3 = group['betas']
 
@@ -155,17 +164,30 @@ class ASAdam(Optimizer):
                 bias_correction2 = 1 - beta2 ** state['step']
                 bias_correction3 = 1 - beta3 ** state['step']
 
-                # add L2
-                if group['weight_decay'] != 0:
-                    grad = grad.add(p, alpha=group['weight_decay'])
-                # add L1
-                if l1 is not None:
-                    if log_scale is None:
-                        # add normal L1 term
-                        grad = grad + group['l1'] * p.sign()
-                    else:
-                        # add hyperbolic L1 term
-                        grad = grad + log_scale * group['l1'] * p.sign() / (p.abs() + log_scale)
+                # work with copy when gradient needs to be modified
+                if l2 != 0 or l1 is not None or l1_log is not None:
+                    grad = grad.clone()
+                # add L2 term
+                if l2 != 0:
+                    grad.add_(p, alpha=group['weight_decay'])
+                # add L1 terms
+                if l1 is not None or l1_log is not None:
+                    l1_for_non_zero = torch.zeros_like(grad)
+                    l1_for_zero = torch.zeros_like(grad)
+                    # add normal L1 term
+                    if l1 is not None:
+                        l1_for_non_zero.add_(l1 * p.sign())
+                        l1_for_zero.add_(l1)
+                    # add hyperbolic L1 term
+                    if l1_log is not None:
+                        l1_for_non_zero.addcdiv_(log_scale * l1_log * p.sign(), p.abs() + log_scale)
+                        l1_for_zero.add_(l1_log)
+                    # simply add for non-zero components
+                    grad_for_non_zero = grad + l1_for_non_zero
+                    # for zero components: reduce magnitude of gradient but don't go below zero (no sign switch)
+                    grad_for_zero = grad.sign() * (grad.abs() - l1_for_zero).clamp(min=0)
+                    # compose effective gradient
+                    grad = torch.where(p == 0, grad_for_zero, grad_for_non_zero)
 
                 # Decay the first and second moment running average coefficient
                 exp_avg.mul_(beta1).add_(grad, alpha=1 - beta1)
@@ -179,7 +201,7 @@ class ASAdam(Optimizer):
                     denom = (exp_avg_sq.sqrt() / math.sqrt(bias_correction2)).add_(group['eps'])
 
                 # active set feature
-                if l1 is not None or glue is not None:
+                if l1 is not None or glue is not None or l1_log is not None:
                     # rename
                     avg_grad = exp_avg
                     avg_sq_grad = exp_avg_sq
@@ -203,11 +225,7 @@ class ASAdam(Optimizer):
                     active = state['active']
                     if min_steps <= state['activation_step']:
                         # activate based on reduced gradient
-                        sticky = 0.
-                        if l1 is not None:
-                            sticky += l1
-                        if glue is not None:
-                            sticky += glue
+                        sticky = 0 if glue is None else glue
                         new_active = torch.logical_and(reduced_abs_grad > sticky, torch.logical_not(active))
                         if new_active.any():
                             state['activation_step'] = 0
@@ -220,7 +238,7 @@ class ASAdam(Optimizer):
                                 # if we were to activate all top dimensions
                                 all_activated = active.clone()
                                 all_activated[selected] = True
-                                # only activate those that surpass the threshold from aboce
+                                # only activate those that surpass the threshold from above
                                 active.logical_or_(torch.logical_and(new_active, all_activated))
                             else:
                                 # activate all
@@ -233,10 +251,10 @@ class ASAdam(Optimizer):
                     # effective gradient
                     eff_grad = safe_grad / (avg_sq_grad_corr.sqrt() + group['eps'])
                     if self.debug:
-                        self.direction = eff_grad.clone()
+                        self.direction = eff_grad.clone().detach()
                     eff_grad.mul_(active)  # zero inactive components
                     # remember old location
-                    old_p = p.detach().clone()
+                    old_p = p.clone()
                     # perform step
                     p.add_(eff_grad, alpha=-group['lr'])
                     # check for sign switches
@@ -248,20 +266,20 @@ class ASAdam(Optimizer):
 
                     # remember values
                     if self.debug:
+                        self.grad = p.grad.clone().detach()
+                        self.grad_reg = grad.clone().detach()
 
-                        self.grad = grad.detach().clone()
+                        self.avg_grad = avg_grad.clone().detach()
+                        self.avg_sq_grad = avg_sq_grad.clone().detach()
+                        self.avg_var = avg_var.clone().detach()
 
-                        self.avg_grad = avg_grad.detach().clone()
-                        self.avg_sq_grad = avg_sq_grad.detach().clone()
-                        self.avg_var = avg_var.detach().clone()
+                        self.avg_grad_corr = avg_grad_corr.clone().detach()
+                        self.avg_sq_grad_corr = avg_sq_grad_corr.clone().detach()
+                        self.avg_var_corr = avg_var_corr.clone().detach()
+                        self.eff_var = eff_var.clone().detach()
+                        self.uncertainty = uncertainty.clone().detach()
 
-                        self.avg_grad_corr = avg_grad_corr.detach().clone()
-                        self.avg_sq_grad_corr = avg_sq_grad_corr.detach().clone()
-                        self.avg_var_corr = avg_var_corr.detach().clone()
-                        self.eff_var = eff_var.detach().clone()
-                        self.uncertainty = uncertainty.detach().clone()
-
-                        self.safe_grad = safe_grad.detach().clone()
+                        self.safe_grad = safe_grad.clone().detach()
                 else:
                     # normal Adam update
                     step_size = group['lr'] / bias_correction1
@@ -269,13 +287,13 @@ class ASAdam(Optimizer):
 
                     # remember values
                     if self.debug:
-                        self.grad = grad.detach().clone()
-                        self.avg_grad = exp_avg.detach().clone()
-                        self.avg_sq_grad = exp_avg_sq.detach().clone()
-                        self.avg_grad_corr = (exp_avg / bias_correction1).detach().clone()
-                        self.avg_sq_grad_corr = (exp_avg_sq / bias_correction2).detach().clone()
+                        self.grad = grad.clone().detach()
+                        self.avg_grad = exp_avg.clone().detach()
+                        self.avg_sq_grad = exp_avg_sq.clone().detach()
+                        self.avg_grad_corr = (exp_avg / bias_correction1).clone().detach()
+                        self.avg_sq_grad_corr = (exp_avg_sq / bias_correction2).clone().detach()
 
-                        self.direction = (exp_avg / denom / bias_correction1).detach().clone()
+                        self.direction = (exp_avg / denom / bias_correction1).clone().detach()
 
 
         return loss
